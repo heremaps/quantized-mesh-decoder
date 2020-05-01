0e7971e477d47bec5aa35a225a8a0c6f6763fb77
Kyle Barron <kylebarron2@gmail.com>
2020-05-01 14:18:26 -0600

    Run standard
    Signed-off-by: Kyle Barron <kylebarron2@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,18 @@
-import { terser } from "rollup-plugin-terser";
+import { terser } from 'rollup-plugin-terser'
 
 const config = (file, plugins = []) => ({
-  input: "src/index.js",
+  input: 'src/index.js',
   output: {
-    name: "quantized-mesh-decoder",
-    format: "umd",
+    name: 'quantized-mesh-decoder',
+    format: 'umd',
     indent: false,
-    exports: "named",
-    file,
+    exports: 'named',
+    file
   },
-  plugins,
-});
+  plugins
+})
 
 export default [
-  config("dist/quantized-mesh-decoder.js"),
-  config("dist/quantized-mesh-decoder.min.js", [terser()]),
-];
+  config('dist/quantized-mesh-decoder.js'),
+  config('dist/quantized-mesh-decoder.min.js', [terser()])
+]
